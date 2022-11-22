a43cecef6e563a22f023403c2580c871cfc1ed80
kyoohyun <kgh2120@naver.com>
2022-11-22 23:29:16 +0900

    CHORE : Service class Transactional 추가

--- src/main/java/com/mju/insuranceCompany/service/user/service/UserService.java
+++ src/main/java/com/mju/insuranceCompany/service/user/service/UserService.java
@@ -12,10 +12,11 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-@Slf4j
+@Slf4j @Transactional
 public class UserService implements UserDetailsService {
 
     private final UserRepository userRepository;
