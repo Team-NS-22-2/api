d38fbbe651d3db04f56875362bddee6f3a45f791
kyoohyun <kgh2120@naver.com>
2022-11-29 15:32:29 +0900

    CHORE : 예외 메세지 수정

--- src/main/java/com/mju/insuranceCompany/service/customer/exception/CustomerErrorCode.java
+++ src/main/java/com/mju/insuranceCompany/service/customer/exception/CustomerErrorCode.java
@@ -12,7 +12,7 @@ public enum CustomerErrorCode implements ErrorCode {
     CONTRACT_OF_CUSTOMER_NOT_FOUND(HttpStatus.NOT_FOUND, "고객에게 가입된 계약 정보를 찾을 수 없습니다"),
     CUSTOMER_NOT_FOUND(HttpStatus.NOT_FOUND, "고객 정보가 존재하지 않습니다."),
 
-    PAYMENT_LIST_EMPTY(HttpStatus.NOT_FOUND, "등록된 결제수단이 존재하지 않습니다.")
+    PAYMENT_LIST_EMPTY(HttpStatus.NOT_FOUND, "등록된 결제수단이 존재하지 않습니다. \n결제 수단 추가 창을 열겠습니다.")
 
     ;
 
