e1c91bbd54ddc32d9511dc5163a0d304cc67f481
Seungho <hsh47607@naver.com>
2022-12-07 15:11:26 +0900

    refactor: 1. feature pay compensation / 2. string constants

---
+++ src/main/java/com/mju/insuranceCompany/global/constant/StringConstant.java
@@ -0,0 +1,12 @@
+package com.mju.insuranceCompany.global.constant;
+
+public class StringConstant {
+
+    /** S3 Accident document file directory */
+    public static final String S3_ACC_DOC_DIRECTORY = "acc_doc";
+    /** S3 Sales auth file directory */
+    public static final String S3_SALES_AUTH_DIRECTORY = "sales_auth";
+
+    public static final String MESSAGE_DO_NOT_NEED_PAY_COMPENSATION = "고객 과실이 없으므로 보상금을 지급하지 않습니다.";
+
+}
